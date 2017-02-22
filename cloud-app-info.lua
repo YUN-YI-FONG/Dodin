1d7a692d8371d50e8cef742173fdf4f28de46c55
kevin999123 <karta0832590@gmail.com>
2017-02-22 11:34:04 +0800

    修改錯誤
    修改錯誤

--- cloud-app-info.lua
+++ cloud-app-info.lua
@@ -1,13 +1,5 @@
 return{
-<<<<<<< HEAD
     MasterAddress = "169.56.74.163:5055",
-=======
-<<<<<<< HEAD
-    MasterAddress = "169.56.74.163:5055",
-=======
-    MasterAddress = "ns.exitgames.com:5058",
->>>>>>> origin/master
->>>>>>> origin/master
     AppId = "7e29c72b-0ac1-474f-9355-0f931e7d61c8",
     AppVersion =  "1.0",
 --id?版本?
